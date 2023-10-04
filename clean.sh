dfa1ac94fa67f7ff5f6bff3d8937d44be68da8ba
shuailiu1990 <yuyefeng28@163.com>
2023-10-04 22:36:16 +0800

    update

--- clean.sh
+++ clean.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
-#for target in ./*
-#do
-#    if test -f $target
-#    then
-#        echo $target 是文件
-#    fi
-#    if test -d $target
-#    then
-#        echo $target 是目录
-#    fi
-#done
 
 for target in ./*
 do
     echo $target
-    if test -d $target
-    then
-        if [ $target == "./cv" ] || [ $target == "./cv-chinese" ] || [ $target == "./cv-template" ] || [ $target == "./publication-list" ] || [ $target == "./recommendation-letter" ]
-        then
+    if [ -d $target ]; then
+        if [ $target == "./cv" ] || [ $target == "./cv-chinese" ] || [ $target == "./cv-template" ] || [ $target == "./publication-list" ] || [ $target == "./recommendation-letter" ]; then
             cd $target
             make clean
             cd ..
