9e9405c76d80e4e6d7061cfccf620e19734eff38
shuailiu1990 <yuyefeng28@163.com>
2024-01-13 16:19:34 +0800

    update

--- clean.sh
+++ clean.sh
@@ -4,17 +4,17 @@ for target in ./*; do
 	echo $target
 	if [ -d $target ]; then
 		if [ $target == "./cv" ] || [ $target == "./cv-chinese" ] || [ $target == "./cv-template" ] || [ $target == "./publication-list" ] || [ $target == "./recommendation-letter" ]; then
-			cd $target
-			make clean
-			cd ..
-		else
-			cd $target
-			for subtarget in ./*; do
-				cd $subtarget
-				make clean
+			cd $target &&
+				make clean &&
 				cd ..
-			done
-			cd ..
+		else
+			cd $target &&
+				for subtarget in ./*; do
+					cd $subtarget &&
+						make clean &&
+						cd ..
+				done &&
+				cd ..
 		fi
 	fi
 done
