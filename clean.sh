93831cc2cb4a02c68828956d102262dd0e7264aa
shuailiu1990 <shuai.liu.1990@foxmail.com>
2024-09-21 11:36:09 +0800

    update

--- clean.sh
+++ clean.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 for target in ./*; do
-	echo $target
-	if [[ -d $target ]]; then
-		if [[ $target == "./cv" ]] || [[ $target == "./cv-chinese" ]] || [[ $target == "./cv-template" ]] || [[ $target == "./publication-list" ]] || [[ $target == "./recommendation-letter" ]]; then
-			echo $target
-			cd $target
-			make clean
-			cd ..
-		else
-			cd $target
-			for subtarget in ./*; do
-				cd $subtarget
-				make clean
-				cd ..
-			done
-			cd ..
-		fi
-	fi
+    echo $target
+    if [[ -d $target ]]; then
+        if [[ $target == "./cv-english" ]] || [[ $target == "./cv-chinese" ]] || [[ $target == "./cv-template" ]] || [[ $target == "./publication-list" ]] || [[ $target == "./recommendation-letter" ]]; then
+            echo $target
+            cd $target
+            make clean
+            cd ..
+        else
+            cd $target
+            for subtarget in ./*; do
+                cd $subtarget
+                make clean
+                cd ..
+            done
+            cd ..
+        fi
+    fi
 done
